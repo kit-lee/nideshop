af6622bb091e92ae9fbf32f54da2a464091dbc9b
Kit <kit_21cn@21cn.com>
2018-08-16 15:03:06 +0800

    bug fix

--- src/api/controller/brand.js
+++ src/api/controller/brand.js
@@ -2,8 +2,8 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
   async listAction() {
-    const model = this.model('brand');
-    const data = await model.field(['id', 'name', 'floor_price', 'app_list_pic_url']).order({id: 'asc', sort_order: 'asc'}).page(this.get('page') || 1, this.get('size') || 10).countSelect();
+    const data = await this.model('brand').where({is_show: true}).order({id: 'asc', sort_order: 'asc'})
+      .page(this.get('page') || 1, this.get('size') || 10).countSelect();
 
     return this.success(data);
   }
